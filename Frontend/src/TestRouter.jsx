e207a60b860e7bd1ccac7fac2cf80f8a43eceffb
malaikak077 <143001882+malaikak077@users.noreply.github.com>
2024-09-07 10:19:52 +0500

    Footer done

--- Frontend/src/TestRouter.jsx
+++ Frontend/src/TestRouter.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/ui/Navbar";
 import HomeCarousel from "./components/ui/HomeCarousel";  
 import EventCard from "./components/ui/EventCard";
+import Footer from "./components/ui/Footer";
 function TestRouter() {
   return (
     <Routes>
       <Route path="/" element={<Navbar/>}></Route>
-      <Route path="/Home" element={<HomeCarousel/>}></Route>
+      <Route path="/home" element={<HomeCarousel/>}></Route>
       <Route path="/card" element={<EventCard/>}></Route>
+      <Route path="/Footer" element={<Footer/>}></Route>
 
     </Routes>
   );
