dc662015caebabb827f2057cf0eb54225b47b9c7
malaikak077 <143001882+malaikak077@users.noreply.github.com>
2024-09-07 10:26:32 +0500

    CountdownTimer done

--- Frontend/src/TestRouter.jsx
+++ Frontend/src/TestRouter.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/ui/Navbar";
 import HomeCarousel from "./components/ui/HomeCarousel";  
 import EventCard from "./components/ui/EventCard";
 import Footer from "./components/ui/Footer";
+import CountdownTimer from "./components/ui/CountdownTimer";
 function TestRouter() {
   return (
     <Routes>
@@ -10,6 +11,7 @@ function TestRouter() {
       <Route path="/home" element={<HomeCarousel/>}></Route>
       <Route path="/card" element={<EventCard/>}></Route>
       <Route path="/Footer" element={<Footer/>}></Route>
+      <Route path="/Count" element={<CountdownTimer/>}></Route>
 
     </Routes>
   );
