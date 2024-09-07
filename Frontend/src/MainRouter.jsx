055008736939286eb5e74edcea14ade9329e1efe
Muhammad Umar Shahid <159911362+M-Umar-Shahid@users.noreply.github.com>
2024-09-07 11:38:38 +0500

    using createevent component

--- Frontend/src/MainRouter.jsx
+++ Frontend/src/MainRouter.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import EventPage from "./pages/EventDetailPages";
+import CreateEventpage from "./pages/CreateEventpage";
 function MainRouter() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/event/:id" element={<EventPage />}></Route>
+        <Route path="/event" element={<CreateEventpage />}></Route>
       </Routes>
     </div>
   );
